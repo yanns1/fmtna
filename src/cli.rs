76dc813f7cdbdf115e16a7463b96f866075db402
Yann SALMON <yannsalmon.pro@gmail.com>
2024-05-20 17:08:08 +0200

    have naming conventions be written the same in command-line and config file

--- src/cli.rs
+++ src/cli.rs
@@ -77,16 +77,22 @@ pub struct Cli {
 
 #[derive(ValueEnum, Clone, Debug, Serialize, Deserialize)]
 pub enum NamingConvention {
+    #[serde(rename = "camelCase")]
     #[value(name = "camelCase")]
     CamelCase,
+    #[serde(rename = "kebab-case")]
     #[value(name = "kebab-case")]
     KebabCase,
+    #[serde(rename = "snake_case")]
     #[value(name = "snake_case")]
     SnakeCase,
+    #[serde(rename = "PascalCase")]
     #[value(name = "PascalCase")]
     PascalCase,
+    #[serde(rename = "lower")]
     #[value(name = "lower")]
     Lower,
+    #[serde(rename = "UPPER")]
     #[value(name = "UPPER")]
     Upper,
 }
